2f55c441c467d5349f7077dc3a9ec13f1b50427a
William Schwindt <williammschwindt@gmail.com>
2020-01-23 17:37:16 -0800

    form validation and error messages

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -5,13 +5,14 @@ import axios from 'axios';
 
 
 
+
 const UserForm = ({ errors, touched, values, status }) => {
     return (
             <Form>
-                <Field name="name" type="text" placeholder="Name" />
-                <Field name="email" type="text" placeholder="Email" />
-                <Field name="password" type="text" placeholder="Password" />
-                <label><Field name="Terms" type="checkbox" /></label>
+                <Field name="name" type="text" placeholder="Name" value={values.name}/>
+                <Field name="email" type="text" placeholder="Email" value={values.email}/>
+                <Field name="password" type="text" placeholder="Password" value={values.password}/>
+                <label><Field type="checkbox" name="terms" value={values.terms} /></label>
 
                 <button type="submit">Submit</button>
             </Form>
@@ -19,7 +20,25 @@ const UserForm = ({ errors, touched, values, status }) => {
 }
 
 const FormikUserForm = withFormik({
+    mapPropsToValues() {
+        return {
+            name: "",
+            email: "",
+            password: "",
+            terms: false
+        };
+    },
 
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("This is required"),
+        email: Yup.string().required("This is required"),
+        password: Yup.string().required("This is required"),
+        terms: Yup.bool()
+    }),
+
+    handleSubmit(values) {
+        console.log(values);
+    }
 })(UserForm);
 
 export default FormikUserForm;
