062c3ec7d231d2414ea9200d94fc0569a20e5c6a
William Schwindt <williammschwindt@gmail.com>
2020-01-23 17:56:20 -0800

    data rendering

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -7,7 +7,14 @@ import axios from 'axios';
 
 
 const UserForm = ({ errors, touched, values, status }) => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        status && setUsers(user => [...user, status]);
+    }, [status]);
+
     return (
+        <div>
             <Form>
                 <Field name="name" type="text" placeholder="Name" value={values.name}/>
                 <Field name="email" type="text" placeholder="Email" value={values.email}/>
@@ -16,6 +23,18 @@ const UserForm = ({ errors, touched, values, status }) => {
 
                 <button type="submit">Submit</button>
             </Form>
+
+            {users.map((user, key) => {
+                return (
+                    <div key={user.id}>
+                        <h2>{user.name}</h2>
+                        <p>{user.email}</p>
+                        <p>{user.password}</p>
+                        <p>{user.terms}</p>
+                    </div>
+                )
+            })}
+        </div>
     );
 }
 
@@ -36,8 +55,17 @@ const FormikUserForm = withFormik({
         terms: Yup.bool()
     }),
 
-    handleSubmit(values) {
-        console.log(values);
+    handleSubmit(values, {resetForm, setStatus}) {
+        console.log("Form Values: ", values);
+
+        axios
+            .post("https://reqres.in/api/users", values)
+
+            .then(res => {
+                console.log("Posted Data: ", res);
+                setStatus(res.data);
+                resetForm();
+            })
     }
 })(UserForm);
 
